13c697088e580214ec09215b8914645423569bcf
Evenza <geetisandhyaevenza@gmail.com>
2026-01-27 02:03:11 +0530

    Project Created..

---
+++ server/src/main/java/com/evenza/server/EvenzaServerApplication.java
@@ -0,0 +1,13 @@
+package com.evenza.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvenzaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvenzaServerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/evenza/server/EvenzaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evenza.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvenzaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
